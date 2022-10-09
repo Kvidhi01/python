f940bc0e33b2364113b7030c7f38e0c014e6723a
Kvidhi01 <93852321+Kvidhi01@users.noreply.github.com>
2022-10-09 23:56:24 +0530

    Add files via upload

---
+++ py02.py
@@ -0,0 +1,16 @@
+# working this list
+
+
+
+
+
+
+
+
+names = ["sparsh", "aditya", "namita", "joya", "dhruv", "shipranjali"]
+name = input("Enter the name to check\n")
+
+if name in names:
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
